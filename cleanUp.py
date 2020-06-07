86ca2684f0e7534a8486f91594c6cbaede783960
s1932706 <z.wu-32@sms.ed.ac.uk>
2020-06-07 19:54:37 +0100

    Classify the data based on its class

--- cleanUp.py
+++ cleanUp.py
@@ -3,22 +3,22 @@ import pandas as pd
 for i in range(2006, 2020):
     f = open('original_data/original_attributes_'+str(i)+'.csv', 'r')
     # list contains the title of useless lines
-    list = ['\"Unweighted Base\"', '\" Start month of trip\"', '\"Total\"']
+    list = ['\"unweighted base\"', '\" start month of trip\"', '\"total\"', '\"weighted base\"']
     docs = range(2006, 2020)
-    line = f.readline()
+    line = f.readline().lower()
     input = []
     # read each lines to the list
     while line:
         input.append(line)
-        line = f.readline()
+        line = f.readline().lower()
     # close the file
     f.close()
     output = []
     # For each line in the list, check if it is useless. If not, add it to the output list
     for item in input:
         item = item.split(',')
-        if item[0] == '\"\"' and (item[1] == '\"Total\"' or item[1] == '\"TOTAL\"'):
-            item[0] = '\"Month\"'
+        if item[0] == '\"\"' and item[1] == '\"total\"':
+            item[0] = '\"month\"'
             output.append(','.join(x for x in item))
         elif not item[0] == '\"\"' and len(item) > 1 and not list.__contains__(item[0]):
             output.append(','.join(x for x in item))
@@ -31,5 +31,20 @@ for i in range(2006, 2020):
     data = pd.read_csv('data/visitor_attributes_'+str(i)+'.csv')
     # Drop all columns which all values in column are null
     data = data.dropna(axis=1, how="all")
-    # Write to a new csv file
-    data.to_csv('data/visitor_attributes_'+str(i)+'.csv', index=0)
+    #Drop the repeated range
+    if i in range(2016, 2020):
+        data = data.drop(columns=['55+', '16-34', '35-54'])
+    gender = data[['month', 'male', 'female']]
+    gender.to_csv('data/gender/gender_'+str(i)+'.csv', index=0)
+    age = data[['month', '16-24', '25-34', '35-44', '45-54', '55-64', '65+']]
+    age.to_csv('data/age/age_'+str(i)+'.csv', index=0)
+    social = data[['month', 'a', 'b', 'c1', 'c2', 'd', 'e']]
+    social.to_csv('data/social_class/social_'+str(i)+'.csv', index=0)
+    if i in range(2016, 2020):
+        marry = data[['month', 'married', 'single', 'separated/widowed/divorced']]
+        marry.to_csv('data/marital/marry_'+str(i)+'.csv', index=0)
+        work = data[['month', 'full time', 'part time', 'other']]
+        work.to_csv('data/working/work_'+str(i)+'.csv', index=0)
+    else:
+        chilren = data[['month', 'yes', 'no']]
+        chilren.to_csv('data/children/chilren_'+str(i)+'.csv', index=0)
