5a326037967afd321786d0ba9464cbb7db11918d
s1932706 <z.wu-32@sms.ed.ac.uk>
2020-06-11 01:48:17 +0100

    Add the path

--- clean/cleanUpVisitors.py
+++ clean/cleanUpVisitors.py
@@ -1,11 +1,12 @@
 import pandas as pd
+import os
 # list contains the title of useless lines
 list = ['\"unweighted base\"', '\"weighted base\"', '\"month\"', '\"base: all respondents\"', '\n', '\"all scotland"']
 #open the file and read each line to a list
 for i in range(2011, 2020):
     input = []
     output = []
-    f = open('original_data/original_visitor/original_visitor_'+str(i)+'.csv', 'r')
+    f = open(os.path.dirname(os.getcwd())+'/original_data/original_visitor/original_visitor_'+str(i)+'.csv', 'r')
     line = f.readline().lower()
     # read each lines to the list
     while line:
@@ -20,19 +21,19 @@ for i in range(2011, 2020):
             output.append(','.join(x for x in line))
         elif not line[0] == '\"\"' and len(line) > 1 and not list.__contains__(line[0]):
             output.append(','.join(x for x in line))
-    f = open('transfer/visitor/visitor_'+str(i)+'.csv', 'w')
+    f = open(os.path.dirname(os.getcwd())+'/transfer/visitor/visitor_'+str(i)+'.csv', 'w')
     for line in output:
         f.write(line)
     f.close()
 for i in range(2011, 2016):
-    df = pd.read_csv('transfer/visitor/visitor_'+str(i)+'.csv')
+    df = pd.read_csv(os.path.dirname(os.getcwd())+'/transfer/visitor/visitor_'+str(i)+'.csv')
     df.columns = ['month', 'total', 'male', 'female', '16-24', '25-34', '35-44', '45-54', '55-64', '65+', 'married', 'not married', 'yes', 'no', 'access to car (1+)', 'no access to car (0)', 'ab', 'c1', 'c2', 'de', 'employed/self-employed (full or part time)', 'in full or part time education', 'unemployed/not working', '']
-    df.to_csv('transfer/visitor/visitor_'+str(i)+'.csv', index = 0)
-data = pd.read_csv('transfer/visitor/visitor_2011.csv')
+    df.to_csv(os.path.dirname(os.getcwd())+'/transfer/visitor/visitor_'+str(i)+'.csv', index = 0)
+data = pd.read_csv(os.path.dirname(os.getcwd())+'/transfer/visitor/visitor_2011.csv')
 data['year'] = 2011
 #Combine and classify the CSV files
 for i in range(2012, 2020):
-    df = pd.read_csv('transfer/visitor/visitor_'+str(i)+'.csv')
+    df = pd.read_csv(os.path.dirname(os.getcwd())+'/transfer/visitor/visitor_'+str(i)+'.csv')
     df['year'] = i
     data = pd.concat([data, df])
 #Drop the useless columns
@@ -50,10 +51,10 @@ cars = data[['year', 'month', 'access to car (1+)',	'no access to car (0)']]
 social = data[['year', 'month', 'ab', 'c1', 'c2', 'de']]
 working = data[['year', 'month', 'employed/self-employed (full or part time)',	'in full or part time education', 'unemployed/not working']]
 #Write each CSV file to the disk
-age.to_csv('data/age/age.csv', index=0)
-gender.to_csv('data/gender/gender.csv', index=0)
-married.to_csv('data/married/married.csv', index=0)
-children.to_csv('data/children/children.csv', index=0)
-cars.to_csv('data/cars/cars.csv', index=0)
-social.to_csv('data/social/social.csv', index=0)
-working.to_csv('data/working/working.csv', index=0)
+age.to_csv(os.path.dirname(os.getcwd())+'/data/age/age.csv', index=0)
+gender.to_csv(os.path.dirname(os.getcwd())+'/data/gender/gender.csv', index=0)
+married.to_csv(os.path.dirname(os.getcwd())+'/data/married/married.csv', index=0)
+children.to_csv(os.path.dirname(os.getcwd())+'/data/children/children.csv', index=0)
+cars.to_csv(os.path.dirname(os.getcwd())+'/data/cars/cars.csv', index=0)
+social.to_csv(os.path.dirname(os.getcwd())+'/data/social/social.csv', index=0)
+working.to_csv(os.path.dirname(os.getcwd())+'/data/working/working.csv', index=0)
