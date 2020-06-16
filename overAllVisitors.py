cb12c22a163c53f4592fcc956f0fe15d8bf820b9
s1932706 <z.wu-32@sms.ed.ac.uk>
2020-06-16 01:11:38 +0100

    Draw the overall line chart and save it as a 'PNG' file.

---
+++ overAllVisitors.py
@@ -0,0 +1,17 @@
+from matplotlib import pyplot as plt
+import readFromDatabase as rfd
+#Read data from database
+visitorData = rfd.readFrom('Gender')
+#Add a column named 'Total'
+visitorData['Total'] = visitorData.Male + visitorData.Female
+plt.figure(figsize=(8, 8))
+#Group the data based on 'Year', calculate the number of visitors for each year and generate a new dataframe.
+overAll = visitorData.groupby('Year').Total.sum().reset_index()
+#Draw a line chart to show the overall trend and set labels and titles for the chart.
+plt.plot(overAll['Year'], overAll['Total'], marker='o')
+plt.ylabel('Count(Million)')
+plt.xlabel('Years')
+plt.title('Number of Visitors for Day Visist in Scotland')
+#Save the chart as a 'PNG' file.
+plt.savefig('figures/overall.png')
+plt.show()
