57c541b3decf1dd3191ce46623cf424c8046033e
s1932706 <z.wu-32@sms.ed.ac.uk>
2020-06-17 05:14:07 +0100

    Add a file to draw the figrue of visitors from different age over recent 10 years.

---
+++ ageOverYears.py
@@ -0,0 +1,22 @@
+from matplotlib import pyplot as plt
+import readFromDatabase as rfd
+#Read data from database
+visitorData = rfd.readFrom('Age')
+#Add a column named 'Total'
+plt.figure(figsize=(8, 8))
+#Group the data based on 'Year', calculate the number of visitors for each year and generate a new dataframe.
+overAll = visitorData.groupby('Year')['16-24', '25-34', '35-44', '45-54', '55-64', '65+'].sum().reset_index()
+#Draw a line chart to show the overall trend and set labels and titles for the chart.
+plt.plot(overAll['Year'], overAll['16-24'], marker='o')
+plt.plot(overAll['Year'], overAll['25-34'], marker='s')
+plt.plot(overAll['Year'], overAll['35-44'], marker='v')
+plt.plot(overAll['Year'], overAll['45-54'], marker='p')
+plt.plot(overAll['Year'], overAll['55-64'], marker='+')
+plt.plot(overAll['Year'], overAll['65+'], marker='x')
+plt.legend(['16-24', '25-34', '35-44', '45-54', '55-64', '65+'])
+plt.ylabel('Count(Million)')
+plt.xlabel('Years')
+plt.title('Number of Visitors from Different Age for Day Visist in Scotland')
+#Save the chart as a 'PNG' file.
+plt.savefig('figures/AgeOverYears.png')
+plt.show()
