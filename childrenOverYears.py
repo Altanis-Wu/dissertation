d4900cc3953a8a8cfd4cd15ce7ebdc2994070616
s1932706 <z.wu-32@sms.ed.ac.uk>
2020-06-17 05:18:31 +0100

    Add a file to draw the figrue of visitors who have children over recent 10 years.

---
+++ childrenOverYears.py
@@ -0,0 +1,17 @@
+from matplotlib import pyplot as plt
+import readFromDatabase as rfd
+#Read data from database
+visitorData = rfd.readFrom('Children')
+plt.figure(figsize=(8, 8))
+#Group the data based on 'Year', calculate the number of visitors for each year and generate a new dataframe.
+overAll = visitorData.groupby('Year')['Yes', 'No'].sum().reset_index()
+#Draw a line chart to show the overall trend and set labels and titles for the chart.
+plt.plot(overAll['Year'], overAll['Yes'], marker='o')
+plt.plot(overAll['Year'], overAll['No'], marker='s')
+plt.legend(['Yes', 'No'])
+plt.ylabel('Count(Million)')
+plt.xlabel('Years')
+plt.title('Number of Visitors who Have Children for Day Visist in Scotland')
+#Save the chart as a 'PNG' file.
+plt.savefig('figures/ChildrenOverYears.png')
+plt.show()
