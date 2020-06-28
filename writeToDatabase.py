13a150637a3358b2390f7c835b85b8ae0b785aa3
s1932706 <z.wu-32@sms.ed.ac.uk>
2020-06-29 00:43:29 +0100

    Remove some useless files.Read each file about attributes and actions as dataframe and write them to database.

--- writeToDatabase.py
+++ writeToDatabase.py
@@ -5,7 +5,12 @@ inputFile = ['Age', 'Cars', 'Children', 'Gender', 'Married', 'Social', 'Working'
 #Connect to MySQL database
 connection = create_engine("mysql+pymysql://root@localhost:3306/DissertationDatabase")
 #Read each file as dataframe and write them to database
+"""
 for file in inputFile:
     input = pd.read_csv("data/"+file.lower()+'/'+file.lower()+".csv")
     input.to_sql(file, connection, schema='DissertationDatabase', if_exists='append', index=False)
-
+"""
+#Read each file about attributes and actions as dataframe and write them to database
+for file in inputFile:
+    input = pd.read_csv("data/"+file.lower()+'/'+file.lower()+"_action.csv")
+    input.to_sql(file.lower()+"_action", connection, schema='DissertationDatabase', if_exists='append', index=False)
