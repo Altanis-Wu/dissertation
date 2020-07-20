1e4c4c173b1e1eaa27eb71ede0dd145a3807f4bb
s1932706 <z.wu-32@sms.ed.ac.uk>
2020-07-20 09:58:49 +0100

    Modify the function to read the visitor data from the database. We should pass two parameters named table name and type of attribute now.

--- readFromDatabase.py
+++ readFromDatabase.py
@@ -4,8 +4,8 @@ from sqlalchemy import create_engine
 #Connect to MySQL database
 connection = create_engine("mysql+pymysql://root@localhost:3306/DissertationDatabase")
 
-def readFrom(tableName):
-    sql = 'select * from ' + tableName + ';'
+def readFrom(tableName, type):
+    sql = 'select * from ' + tableName + ' where Type=\"'+type+'\";'
     return pd.read_sql_query(sql, connection)
 
 def readAction(tableName, type):
