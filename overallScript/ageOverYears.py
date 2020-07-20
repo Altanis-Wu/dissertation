cece6c1be3ad0a23d4e2d67f739ddde48933dae1
s1932706 <z.wu-32@sms.ed.ac.uk>
2020-07-20 10:01:28 +0100

    Modify the code to adjust the new read function.

--- overallScript/ageOverYears.py
+++ overallScript/ageOverYears.py
@@ -2,18 +2,16 @@ from matplotlib import pyplot as plt
 import readFromDatabase as rfd
 import os
 #Read data from database
-visitorData = rfd.readFrom('Age')
+visitorData = rfd.readFrom('visitor', 'age')
 #Add a column named 'Total'
 plt.figure(figsize=(8, 8))
 #Group the data based on 'Year', calculate the number of visitors for each year and generate a new dataframe.
-overAll = visitorData.groupby('Year')['16-24', '25-34', '35-44', '45-54', '55-64', '65+'].sum().reset_index()
+overAll = visitorData.groupby(['Year', 'Attribute']).sum().reset_index()
+age=['16-24', '25-34', '35-44', '45-54', '55-64', '65+']
 #Draw a line chart to show the overall trend and set labels and titles for the chart.
-plt.plot(overAll['Year'], overAll['16-24'], marker='o')
-plt.plot(overAll['Year'], overAll['25-34'], marker='s')
-plt.plot(overAll['Year'], overAll['35-44'], marker='v')
-plt.plot(overAll['Year'], overAll['45-54'], marker='p')
-plt.plot(overAll['Year'], overAll['55-64'], marker='+')
-plt.plot(overAll['Year'], overAll['65+'], marker='x')
+for x in age:
+    data=overAll[overAll['Attribute']==x]
+    plt.plot(data['Year'], data['Count'], marker='o')
 plt.legend(['16-24', '25-34', '35-44', '45-54', '55-64', '65+'])
 plt.ylabel('Count(Million)')
 plt.xlabel('Years')
