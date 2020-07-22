6c02bf171a23e8eebe264323af8ecd7c7bb2b52a
s1932706 <z.wu-32@sms.ed.ac.uk>
2020-07-22 23:18:31 +0100

    Read different attribute data from database and calculate and retrieve total number of visitors for each year.

--- correlation.py
+++ correlation.py
@@ -1,4 +1,11 @@
 import readFromDatabase as rfd
 
-data=rfd.readFrom('visitor', 'age')
-print(data)
+ageData=rfd.readFrom('visitor', 'age')
+genderData=rfd.readFrom('visitor', 'gender')
+carsData=rfd.readFrom('visitor', 'cars')
+childrenData=rfd.readFrom('visitor', 'children')
+marriedData=rfd.readFrom('visitor', 'married')
+socialData=rfd.readFrom('visitor', 'social')
+workingData=rfd.readFrom('visitor', 'working')
+total=ageData.groupby('Year').sum().reset_index()['Count']
+print(total)
