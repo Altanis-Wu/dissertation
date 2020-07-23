b6de6c814df8766d1a7c4010224cd28838228521
s1932706 <z.wu-32@sms.ed.ac.uk>
2020-07-23 01:02:27 +0100

    Calculate the correlation between attributes of visitors and total number of visitors.

--- correlation.py
+++ correlation.py
@@ -8,4 +8,10 @@ marriedData=rfd.readFrom('visitor', 'married')
 socialData=rfd.readFrom('visitor', 'social')
 workingData=rfd.readFrom('visitor', 'working')
 total=ageData.groupby('Year').sum().reset_index()['Count']
-print(total)
+Data=[ageData, genderData, carsData, childrenData, marriedData, socialData, workingData]
+names=['age', 'gender', 'cars', 'children', 'married', 'social', 'working']
+for i in range(len(Data)):
+    tem=Data[i].groupby(['Year', 'Attribute']).sum().reset_index()
+    tem=tem.pivot(index='Year', columns='Attribute', values='Count').reset_index().drop(columns=['Year'])
+    tem['total']=total
+    tem.corr().to_csv('correlation/'+names[i]+'_cor.csv')
