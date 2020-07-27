d580910e1c697b90017458b522e828d296c07095
s1932706 <z.wu-32@sms.ed.ac.uk>
2020-07-27 23:54:04 +0100

    Updates the time series model for each type of visitor.

---
+++ timeSeries.md
@@ -0,0 +1,7 @@
+# Linear Regression on Time Series
+|Category| X | Model | R-squared |
+|:------:|:---:|:----:|:-------:|
+|Gender|female|y = 3516.3 + -1.7067x|0.506|
+|Cars|access to car (1+)|y = 3679.4 + -1.7667x|0.283|
+|Married|Married|y = 5802.8 + -2.8367x|0.715|
+|working|employed/self-employed (full or part time)|y = 6751.8 + -3.3083x|0.478|
--- timeSeries.py
+++ timeSeries.py
@@ -4,7 +4,28 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 import statsmodels.api as sm
 
-genderData=rfd.readCertainVisitor('visitor', 'gender', 'female')
-carData=rfd.readCertainVisitor('visitor', 'cars', 'access to car (1+)')
-marriedData=rfd.readCertainVisitor('visitor', 'married', 'married')
-workingData=rfd.readCertainVisitor('visitor', 'working', 'employed/self-employed (full or part time)')
+list=[('gender', 'female'), ('cars', 'access to car (1+)'), ('married', 'married'),
+      ('working', 'employed/self-employed (full or part time)')]
+for item in list:
+    plt.figure(figsize=(8, 8))
+    data=rfd.readCertainVisitor('visitor', item[0], item[1]).groupby('Year').sum().reset_index()
+    plt.scatter(data['Year'], data['Count'])
+    x=data['Year'].values.reshape(-1, 1)
+    y=data['Count'].values.reshape(-1, 1)
+    reg=LinearRegression()
+    reg.fit(x, y)
+    print(item)
+    print("The linear model is: y = {:.5} + {:.5}x".format(reg.intercept_[0], reg.coef_[0][0]))
+    predictions = reg.predict(x)
+    plt.plot(data['Year'], predictions)
+    x=data['Year']
+    y=data['Count']
+    x2 = sm.add_constant(x)
+    est = sm.OLS(y, x2)
+    est2 = est.fit()
+    print(est2.summary())
+    plt.xlabel('Year')
+    plt.ylabel('Number of Visitors')
+    plt.title('Number of visistors in '+item[1])
+    plt.savefig('timeSeries/'+item[0]+'_time_series.png')
+    plt.show()
