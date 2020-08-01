1815b5f24b9759e0ecc60aeba3c68e4f9da90c44
s1932706 <z.wu-32@sms.ed.ac.uk>
2020-08-01 17:48:55 +0100

    Add a file to build ARIMA model for prediction.

---
+++ ARIMAModel.py
@@ -0,0 +1,14 @@
+import readFromDatabase as rfd
+import matplotlib.pyplot as plt
+import pandas as pd
+import math
+
+timeData=rfd.readFrom('visitor', 'age').groupby(['Year', 'Month']).sum().reset_index()
+rollMean=timeData['Count'].rolling(window=12).mean()
+plt.plot(range(len(timeData['Count'])), timeData['Count'])
+rollMean.plot()
+rollStd=timeData['Count'].rolling(window=12).std()
+rollStd.plot()
+plt.legend(['Original', 'Rolling Mean', 'Rolling Standard Devitation'], loc='best')
+plt.title('Rolling Mean & Standard Deviation')
+plt.show()
